4439b1da7d993fd50ff2450da4c3471e6d0a992b
Jan Ohlberger <janohl@uw.edu>
2024-03-18 11:48:25 -0700

    added progress bar

--- R/run_scenarios.R
+++ R/run_scenarios.R
@@ -4,7 +4,7 @@
 ##                                                                        ##
 ##========================================================================##
 ##===============================================================## packages
-pkgs<-c("here","readxl","openxlsx","dplyr","gtools","faraway","RColorBrewer","gsl","nlme","matrixcalc","data.table","dlm")
+pkgs<-c("here","readxl","openxlsx","dplyr","gtools","faraway","RColorBrewer","gsl","nlme","matrixcalc","data.table","dlm","progress")
 if(length(setdiff(pkgs,rownames(installed.packages())))>0) { install.packages(setdiff(pkgs,rownames(installed.packages())), dependencies=TRUE) }
 invisible(lapply(pkgs,library,character.only=T))
 ##==============================================================## functions
@@ -13,31 +13,33 @@ invisible(sapply(FUN=source,paste0(here::here(),"/R/functions/",fxn)))
 ##==============================================================## scenarios
 scen<-data.frame(read_excel("R/scenarios.xlsx")) ## list of all scenarios
 nscen<-dim(scen)[1] ## number of scenarios
-niter<-10 ## iterations per scenario
+niter<-100 ## iterations per scenario
+print(paste0("time estimate: ",round((niter*nscen/20)/60,2)," hours"))
 ##===========================================================## output lists
 output.list<-replicate(nscen,list(replicate(niter,list())))
 para.list<-sr_sim.list<-fec.list<-egg.list<-S_msy.list<-data.list<-obs.list<-output.list
 ##==========================================## loop scenarios and iterations
-print(paste0("time estimate: ",round((niter*nscen/20)/60,2)," hours"))
+pb<-progress_bar$new(total=niter*nscen)
 # seed.list<-sample(seq(1e6),niter*nscen,replace=F) ## random seed
 seed.list<-seq(niter*nscen) ## reproducible seed for comparing test runs
 start.time<-Sys.time()
 for(j in 1:nscen) { 
-for(k in 1:niter) {
-seednum<-seed.list[k] ## same iteration seeds for each scenario
-print(paste0("scenario=",j," iteration=",k," seed=",seednum))
-parms.list<-source("R/functions/parameters.R")$value
-mod.out<-try(run_model(parms.list=parms.list))
-if(class(mod.out)!="try-error") {
-para.list[[j]][[k]]<-mod.out$para ## true alpha and beta parameters
-sr_sim.list[[j]][[k]]<-mod.out$sr_sim ## SR estimates on simulated data
-fec.list[[j]][[k]]<-mod.out$fec	## change in fecundity over time
-egg.list[[j]][[k]]<-mod.out$egg	## change in eggmass over time
-S_msy.list[[j]][[k]]<-mod.out$S_msy ## S_msy estimates
-data.list[[j]][[k]]<-mod.out$data ## simulated data
-obs.list[[j]][[k]]<-mod.out$obs	## observations
-} ## end if statement
-} ## end k loop
+  for(k in 1:niter) {
+    seednum<-seed.list[k] ## same iteration seeds for each scenario
+    # print(paste0("scenario=",j," iteration=",k," seed=",seednum))
+    parms.list<-source("R/functions/parameters.R")$value
+    mod.out<-try(run_model(parms.list=parms.list))
+    if(class(mod.out)!="try-error") {
+      para.list[[j]][[k]]<-mod.out$para ## true alpha and beta parameters
+      sr_sim.list[[j]][[k]]<-mod.out$sr_sim ## SR estimates on simulated data
+      fec.list[[j]][[k]]<-mod.out$fec	## change in fecundity over time
+      egg.list[[j]][[k]]<-mod.out$egg	## change in eggmass over time
+      S_msy.list[[j]][[k]]<-mod.out$S_msy ## S_msy estimates
+      data.list[[j]][[k]]<-mod.out$data ## simulated data
+      obs.list[[j]][[k]]<-mod.out$obs	## observations
+    } ## end if statement
+    pb$tick()
+  } ## end k loop
 } ## end j loop
 ##==========================================================## true run time
 end.time<-Sys.time()
